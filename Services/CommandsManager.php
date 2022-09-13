a95ce2a277c949e646ddd2fa553f7bace764d18f
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2022-09-13 15:39:48 +0200

    Log

--- Services/CommandsManager.php
+++ Services/CommandsManager.php
@@ -21,17 +21,20 @@ class CommandsManager
     public function warm($args, $assoc_args)
     {
         if (empty($assoc_args['posts'])) {
-            $posts_to_get = 10;
+            $posts_to_get = 100;
             $count_posts = 0;
             $posts_ids = [];
             $offset = 0;
 
             $query = $this->attachmentsTableManager->getPosts($posts_to_get, $offset);
             if (!empty($query)) {
+                output_h1('Found ' . $query->found_posts . ' posts to analyse');
+                $posts_ids = array_merge($posts_ids, $query->posts);
+
                 while ($count_posts < $query->found_posts) {
-                    $count_posts = $count_posts + $query->post_count;
+                    $count_posts += $query->post_count;
                     $posts_ids = array_merge($posts_ids, $query->posts);
-                    $offset = $offset + $query->post_count;
+                    $offset += $query->post_count;
                     $query = $this->attachmentsTableManager->getPosts($posts_to_get, $offset);
                 }
             }
@@ -41,9 +44,12 @@ class CommandsManager
 
         if (!empty($posts_ids)) {
             $field_names = $this->attachmentsTableManager->getAttachmentsFieldNames();
+            $i = 0;
             foreach ($posts_ids as $post_id) {
                 do_action('woody_async_add', 'get_attachments_by_post', ['post_id' => $post_id, 'field_names' => $field_names]);
+                $i++;
             }
+            output_h1($i . ' async created');
         }
     }
 }
