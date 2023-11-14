dac8b8d39e138bb92a2ffc326e260bc1935d5fa5
Maxime Fiquet <maxime.fiquet@raccourci.fr>
2023-11-14 17:52:44 +0100

    [TCK64820] add expiration date on CSV export

--- Services/AttachmentsDataExport.php
+++ Services/AttachmentsDataExport.php
@@ -79,7 +79,6 @@ class AttachmentsDataExport
 
     public function attachmentsDoExport($args)
     {
-        output_log('lancement de la fonction', '');
         output_h1('Do attachments export');
         if ($args['request_args'] && $args['fields']) {
             // On récupère tous les attachments en fonctions des arguments passés(mimetype, lang)
@@ -87,7 +86,6 @@ class AttachmentsDataExport
             $count_posts = 0;
             output_h2('Requesting attachments');
             $query = $this->attachmentsQuery($args['request_args']);
-            output_log($query, 'query');
             output_log(sprintf('0/%s ', $query->found_posts));
 
             // Tant que l'on a pas récupéré la totalité des attachments correspondant à la requête ($query->found_posts),
@@ -104,10 +102,8 @@ class AttachmentsDataExport
 
             // Une fois les attachments récupérés, on convertit le tableau en un fichier csv que l'on stocke dans les uploads du site initiateur
             if (!empty($attachments)) {
-                output_log('there is attachments', '');
                 $time = time();
                 $filespath = $this->arrayToCsv($attachments, $time);
-                output_log($filespath, 'lien du fichier');
                 if ($filespath) {
                     // On réucpère la liste des fichiers existants pour la mettre à jour.
                     // Cette liste nous servira à afficher les liens de téléchargement
@@ -171,17 +167,17 @@ class AttachmentsDataExport
         $post_fields = $custom_fields['post_fields'];
         $acf_fields = $custom_fields['acf_fields'];
 
-        if (!empty($posts)) {
+        if (!empty($fields)) {
             foreach ($posts as $post) {
-                foreach ($post_fields as $post_field) {
-                    if (in_array($post_field, $fields)) {
-                        $return[$post->ID][$post_field] = $post->{$post_field};
+                foreach ($post_fields as $key => $post_field) {
+                    if (in_array($key, $fields)) {
+                        $return[$post->ID][$key] = $post->{$key};
                     }
                 }
 
-                foreach ($acf_fields as $acf_field) {
-                    if (in_array($acf_field, $fields)) {
-                        $return[$post->ID][$acf_field] = get_field($acf_field, $post->ID);
+                foreach ($acf_fields as $key => $acf_field) {
+                    if (in_array($key, $fields)) {
+                        $return[$post->ID][$key] = get_field($key, $post->ID);
                     }
                 }
 
